374d4f2f434e892eeb61d43bf1ef55ec068cd462
Taixing Bi <taixingbi@taixings-mbp.lan>
2022-03-02 20:51:38 -0500

    s

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -5,15 +5,16 @@ import hello.http.HttpWebClient;
 import hello.http.httpClient.HttpPut;
 
 public class App {
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
         System.out.println("----------------app------------------");
 //       HttpClientRequest.getSync();
 
 //        HttpWebClient.get();
 //        HttpApache.get();
+        String data = "{\n  \"Id\": 12345,\n  \"Customer\": \"John Smith\",\n  \"Quantity\": 1,\n  \"Price\": 10.00\n}";
 
-        HttpPut.process();
+        HttpPut.put(data);
         System.out.println("----------------app end------------------");
     }
 
--- src/main/java/hello/http/httpClient/HttpPut.java
+++ src/main/java/hello/http/httpClient/HttpPut.java
@@ -1,28 +1,56 @@
 package hello.http.httpClient;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class HttpPut {
     static String uri= "https://httpbin.org/put";
 
-    public static void process() throws Exception {
-        String data = "{\n  \"Id\": 12345,\n  \"Customer\": \"John Smith\",\n  \"Quantity\": 1,\n  \"Price\": 10.00\n}";
-
-        URL url = new URL(uri);
-        HttpURLConnection http = (HttpURLConnection)url.openConnection();
-        http.setRequestMethod("PUT");
-        http.setDoOutput(true);
-        http.setRequestProperty("Content-Type", "application/json");
+    public static void put(String data)   {
+        HttpURLConnection http = httpConfig();
 
         byte[] out = data.getBytes(StandardCharsets.UTF_8);
-        OutputStream stream = http.getOutputStream();
-        stream.write(out);
+        OutputStream stream = null;
 
-        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        try {
+            stream = http.getOutputStream();
+            stream.write(out);
+            System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         http.disconnect();
+    }
+
+    private static HttpURLConnection httpConfig() {
+        URL url = null;
+        try {
+            url = new URL(uri);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        HttpURLConnection http = null;
+        try {
+            http = (HttpURLConnection)url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            http.setRequestMethod("PUT");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        http.setDoOutput(true);
+        http.setRequestProperty("Content-Type", "application/json");
+        return http;
     }
 }
